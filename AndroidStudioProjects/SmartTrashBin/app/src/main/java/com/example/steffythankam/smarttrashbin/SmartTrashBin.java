3edc76d0452b183db3e8db5505ec6578f6450324
steffytw <steffythankamwilson@cs.ajce.in>
2019-03-01 10:42:10 +0530

    first commit

---
+++ AndroidStudioProjects/SmartTrashBin/app/src/main/java/com/example/steffythankam/smarttrashbin/SmartTrashBin.java
@@ -0,0 +1,59 @@
+package com.example.steffythankam.smarttrashbin;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SmartTrashBin extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText username;
+    private EditText password;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart_trash_bin);
+
+        mAuth = FirebaseAuth.getInstance();
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = username.getText().toString();
+                String pass = password.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener((Activity) getApplicationContext(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Log.d("Steffy","Successful");
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("Steffy", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(SmartTrashBin.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }}
+                            });
+            }
+        });
+    }
+}
