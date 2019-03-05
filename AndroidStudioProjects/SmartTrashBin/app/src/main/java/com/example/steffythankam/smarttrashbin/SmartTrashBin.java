8a8b4636c0dcf662114a1bb4f42b2edeb754cd96
Tophia <48026646+Tophia@users.noreply.github.com>
2019-03-05 12:49:12 +0530

    Updated XML

--- AndroidStudioProjects/SmartTrashBin/app/src/main/java/com/example/steffythankam/smarttrashbin/SmartTrashBin.java
+++ AndroidStudioProjects/SmartTrashBin/app/src/main/java/com/example/steffythankam/smarttrashbin/SmartTrashBin.java
@@ -1,6 +1,7 @@
 package com.example.steffythankam.smarttrashbin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,52 +12,103 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+//import com.google.android.gms.auth.api.signin
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SmartTrashBin extends AppCompatActivity {
+public class SmartTrashBin extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
     private EditText username;
     private EditText password;
     private Button login;
+    private GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smart_trash_bin);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//        username = (EditText) findViewById(R.id.username);
+//        password = (EditText) findViewById(R.id.password);
+//        login = (Button) findViewById(R.id.button);
+//
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String email = username.getText().toString();
+//                String pass = password.getText().toString();
+//                mAuth.signInWithEmailAndPassword(email, pass)
+//                        .addOnCompleteListener(SmartTrashBin.this, new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                if (task.isSuccessful()) {
+//                                    // Sign in success, update UI with the signed-in user's information
+//                                    FirebaseUser user = mAuth.getCurrentUser();
+//                                    Log.d("Steffy","Successful");
+//
+//                                } else {
+//                                    // If sign in fails, display a message to the user.
+//                                    Log.w("Steffy", "signInWithEmail:failure", task.getException());
+//                                    Toast.makeText(SmartTrashBin.this, "Authentication failed.",
+//                                            Toast.LENGTH_SHORT).show();
+//                                }}
+//                        });
+//            }
+//        });
+//    }
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 1);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 1) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
-        mAuth = FirebaseAuth.getInstance();
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        login = (Button) findViewById(R.id.button);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = username.getText().toString();
-                String pass = password.getText().toString();
-                mAuth.signInWithEmailAndPassword(email, pass)
-                        .addOnCompleteListener(SmartTrashBin.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    Log.d("Steffy","Successful");
-
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("Steffy", "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(SmartTrashBin.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }}
-                        });
-            }
-        });
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Debug", "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+        }
     }
 }
