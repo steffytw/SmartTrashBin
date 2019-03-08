f8c0b3b2a7f3a2f9e41857b327f296c9948bfffd
steffytw <steffythankamwilson@cs.ajce.in>
2019-03-08 10:26:22 +0530

    Secondpage

---
+++ app/src/main/java/com/example/steffythankam/smarttrashbin/Activity2.java
@@ -0,0 +1,18 @@
+package com.example.steffythankam.smarttrashbin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+    }
+
+    public void show_odour_status(View view) {
+
+    }
+}
--- app/src/main/java/com/example/steffythankam/smarttrashbin/odourlevel.java
+++ app/src/main/java/com/example/steffythankam/smarttrashbin/odourlevel.java
@@ -3,11 +3,11 @@ package com.example.steffythankam.smarttrashbin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Activity_2 extends AppCompatActivity {
+public class odourlevel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_odourlevel);
     }
 }
