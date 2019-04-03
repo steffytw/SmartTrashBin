c1e31c961eb030680a108d2eb90e40fdeadda258
steffytw <steffythankamwilson@cs.ajce.in>
2019-04-03 15:52:01 +0530

    image

--- app/src/main/java/com/example/steffythankam/smarttrashbin/binstatus.java
+++ app/src/main/java/com/example/steffythankam/smarttrashbin/binstatus.java
@@ -2,12 +2,49 @@ package com.example.steffythankam.smarttrashbin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class binstatus extends AppCompatActivity {
+    private static final String TAG ="binstatus";
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    ProgressBar pgbar1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        database = FirebaseDatabase.getInstance();
+        myRef  = database.getReference("distance");
+        pgbar1=findViewById(R.id.progressBar2);
+        FirebaseApp.initializeApp(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_binstatus);
+    }
+    public void show_bin_status(View view) {
+        myRef.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                pgbar1.setProgress(Integer.parseInt(value));
+            }
+
+
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
 }
