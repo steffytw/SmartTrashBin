8708a0c7e177a2ad69d924576f1a2d6241f7cf31
steffytw <steffythankamwilson@cs.ajce.in>
2019-04-30 12:12:39 +0530

    odour and distance progress bar

--- app/src/main/java/com/example/steffythankam/smarttrashbin/odourlevel.java
+++ app/src/main/java/com/example/steffythankam/smarttrashbin/odourlevel.java
@@ -40,6 +40,7 @@ public class odourlevel extends AppCompatActivity {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 //String value = dataSnapshot.getValue(String.class);
+
                 String value = dataSnapshot.getValue(String.class);
 
                 //String val = ;
