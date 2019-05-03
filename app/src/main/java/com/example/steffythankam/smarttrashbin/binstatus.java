e29a051ca3eabf9cbc4b0c76b8738fa0b8f34cc7
steffytw <steffythankamwilson@cs.ajce.in>
2019-05-03 09:43:52 +0530

    data retrieval from firebase

--- app/src/main/java/com/example/steffythankam/smarttrashbin/binstatus.java
+++ app/src/main/java/com/example/steffythankam/smarttrashbin/binstatus.java
@@ -1,5 +1,6 @@
 package com.example.steffythankam.smarttrashbin;
 
+import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
@@ -22,46 +24,66 @@ public class binstatus extends AppCompatActivity {
     private static final String TAG ="binstatus";
 
 
-    FirebaseDatabase database;
-    DatabaseReference myRef;
-    ProgressBar pgbar1;
+    FirebaseDatabase firebaseDatabase;
 
-
+    TextView textViewDistance;
+    private ProgressDialog dialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        database = FirebaseDatabase.getInstance();
-        myRef  = database.getReference("smarttrashbin-b190a").child("Distance");
-        String val = myRef.toString();
-        System.out.println(val);
-        Log.d(TAG, "Value is: " + val);
-        pgbar1=findViewById(R.id.progressBar2);
-        FirebaseApp.initializeApp(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_binstatus);
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Please wait...");
+        dialog.setCancelable(false);
+
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase  = FirebaseDatabase.getInstance();
+
+        textViewDistance = findViewById(R.id.tv_distance);
+
+
+        dialog.show();
         show_bin_status();
     }
     public void show_bin_status() {
-        myRef.addValueEventListener(new ValueEventListener() {
+        firebaseDatabase.getReference("message").addValueEventListener(new ValueEventListener() {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 //String value = dataSnapshot.getValue(String.class);
-                String value = dataSnapshot.getValue(String.class);
+                String message = dataSnapshot.getValue().toString();
 
                 //String val = ;
-                //Log.d(TAG, "Value is: " + value);
+                Log.d(TAG, "Message is ===================================: " + message);
+                //textViewMessage.setText("Message : "+message);
                 //pgbar1.setProgress(Integer.parseInt(value));
             }
-
-
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+        firebaseDatabase.getReference("Distance").addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = dataSnapshot.getValue(String.class);
+                String distance = dataSnapshot.getValue().toString();
 
+                //String val = ;
+                Log.d(TAG, "Distance is ===================================: " + distance);
+                textViewDistance.setText("Distance : "+distance);
+                dialog.dismiss();
+                //pgbar1.setProgress(Integer.parseInt(value));
+            }
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/steffythankam/smarttrashbin/odourlevel.java
+++ app/src/main/java/com/example/steffythankam/smarttrashbin/odourlevel.java
@@ -1,54 +1,68 @@
 package com.example.steffythankam.smarttrashbin;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+
 public class odourlevel extends AppCompatActivity {
     private static final String TAG ="odourlevel";
 
-    FirebaseDatabase database;
-    DatabaseReference myRef;
-    ProgressBar pgbar;
+
+    FirebaseDatabase firebaseDatabase;
+
+    TextView textViewOdour;
+
+
+    private ProgressDialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        database = FirebaseDatabase.getInstance();
-        myRef  = database.getReference("smarttrashbin-b190a").child("odour");
-        String val = myRef.toString();
-        System.out.println(val);
-        Log.d(TAG, "Value is: " + val);
-         pgbar=findViewById(R.id.progressBar2);
-         FirebaseApp.initializeApp(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_odourlevel);
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Please wait...");
+        dialog.setCancelable(false);
+
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase  = FirebaseDatabase.getInstance();
+        textViewOdour = findViewById(R.id.tv_odour);
+
+
+        dialog.show();
         show_odour_level();
     }
     public void show_odour_level() {
-        myRef.addValueEventListener(new ValueEventListener() {
+        firebaseDatabase.getReference("odour").addValueEventListener(new ValueEventListener() {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 //String value = dataSnapshot.getValue(String.class);
-
-                String value = dataSnapshot.getValue(String.class);
+                String odour = dataSnapshot.getValue().toString();
 
                 //String val = ;
-                //Log.d(TAG, "Value is: " + value);
+                Log.d(TAG, "Odour is ===================================: " + odour);
+                textViewOdour.setText("Odour : "+odour);
+                dialog.dismiss();
                 //pgbar1.setProgress(Integer.parseInt(value));
             }
-
-
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException());
